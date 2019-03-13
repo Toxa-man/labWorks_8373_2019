3488caf26d5900c97dd6bd3abaf60a806df76a0d
DSaluyan <43251516+DSaluyan@users.noreply.github.com>
2019-03-13 16:09:47 +0300

    Update lab_1.cpp

--- SaluyanovDE/lab_1/lab_1.cpp
+++ SaluyanovDE/lab_1/lab_1.cpp
@@ -6,127 +6,140 @@ using T = int;
 class vector
 {
 public:
-	vector(unsigned Size_)
-	{
-		Size = Size_;
-		data = new T[Size];
-		for (unsigned i = 0; i < Size; ++i)
-		{
-			data[i] = T();
-		}
-	}
-	vector(unsigned Size_, const T&val)
-	{
-		Size = Size_;
-		data = new T[Size];
-		for (unsigned i = 0; i < Size; ++i)
-		{
-			data[i] = val;
-		}
-	}
-	vector(const vector&vec)
-	{
-		Size = vec.Size;
-		data = new T[Size];
-		memcpy(data, vec.data, sizeof(T)*Size);
-	}
-	unsigned size()
-	{
-		return Size;
-	}
-	void resize(unsigned NewSize)
-	{
-		if (NewSize > Size)
-		{
-			buffer = new T[NewSize];
-			memcpy(buffer, data, sizeof(T)*Size);
-			delete[]data;
-			data = new T[NewSize];
-			memcpy(data, buffer, sizeof(T)*NewSize);
-			delete[]buffer;
-			for (unsigned i = Size; i < NewSize; ++i)
-			{
-				data[i] = 0;
-			}
-			Size = NewSize;
-		}
-		else
-		{
-			buffer = new T[Size];
-			memcpy(buffer, data, sizeof(T)*Size);
-			delete[]data;
-			data = new T[NewSize];
-			memcpy(data, buffer, sizeof(T)*NewSize);
-			delete[]buffer;
-			Size = NewSize;
-		}
-	}
-	bool insert(unsigned pos, const T&val)
-	{
-		if (pos >= Size)
-		{
-			return false;
-		}
-		else
-		{
-			data[pos] = val;
-			return true;
-		}
-	}
-	void push_back(const T&val)
-	{
-		resize(size() + 1);
-		insert(size() - 1, val);
-	}
-	bool contains(const T&val)
-	{
-		for (unsigned i = 0; i < Size; ++i)
-		{
-			if (val == data[i])
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	int find(const T&val)
-	{
-		for (unsigned i = 0; i < Size; ++i)
-		{
-			if (val == data[i])
-			{
-				return i;
-			}
-		}
-		return -1;
-	}
-	T* get_data()
-	{
-		return data;
-	}
-	vector& operator =(const vector&vec)
-	{
-		if (this != &vec)
-			Size = vec.Size;
-		delete[]data;
-		data = new T[Size];
-		memcpy(data, vec.data, sizeof(T)*Size);
-		return *this;
-	}
-	T& operator [](unsigned val)
-	{
-		return data[val];
-	}
-	~vector()
-	{
-		Size = 0;
-		delete[]data;
-	}
+	vector(unsigned Size_);
+	vector(unsigned Size_, const T&val);
+	vector(const vector&vec);
+	unsigned size();
+	void resize(unsigned NewSize);
+	bool insert(unsigned pos, const T&val);
+	void push_back(const T&val);
+	bool contains(const T&val);
+	int find(const T&val);
+	T* get_data();
+	vector& operator =(const vector&vec);
+	T& operator [](unsigned val);
+	~vector();
 private:
 	T*data;
 	T*buffer;
 	unsigned Size;
 };
+vector::vector(unsigned Size_)
+{
+	Size = Size_;
+	data = new T[Size];
+	for (unsigned i = 0; i < Size; ++i)
+	{
+		data[i] = T();
+	}
+}
+vector::vector(unsigned Size_, const T&val)
+{
+	Size = Size_;
+	data = new T[Size];
+	for (unsigned i = 0; i < Size; ++i)
+	{
+		data[i] = val;
+	}
+}
+vector::vector(const vector&vec)
+{
+	Size = vec.Size;
+	data = new T[Size];
+	memcpy(data, vec.data, sizeof(T)*Size);
+}
+unsigned vector::size()
+{
+	return Size;
+}
+void vector::resize(unsigned NewSize)
+{
+	if (NewSize > Size)
+	{
+		buffer = new T[NewSize];
+		memcpy(buffer, data, sizeof(T)*Size);
+		delete[]data;
+		data = new T[NewSize];
+		memcpy(data, buffer, sizeof(T)*NewSize);
+		delete[]buffer;
+		for (unsigned i = Size; i < NewSize; ++i)
+		{
+			data[i] = 0;
+		}
+		Size = NewSize;
+	}
+	else
+	{
+		buffer = new T[Size];
+		memcpy(buffer, data, sizeof(T)*Size);
+		delete[]data;
+		data = new T[NewSize];
+		memcpy(data, buffer, sizeof(T)*NewSize);
+		delete[]buffer;
+		Size = NewSize;
+	}
+}
+bool vector::insert(unsigned pos, const T&val)
+{
+	if (pos >= Size)
+	{
+		return false;
+	}
+	else
+	{
+		data[pos] = val;
+		return true;
+	}
+}
+void vector::push_back(const T&val)
+{
+	resize(size() + 1);
+	insert(size() - 1, val);
+}
+bool vector::contains(const T&val)
+{
+	for (unsigned i = 0; i < Size; ++i)
+	{
+		if (val == data[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+int vector::find(const T&val)
+{
+	for (unsigned i = 0; i < Size; ++i)
+	{
+		if (val == data[i])
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+T* vector::get_data()
+{
+	return data;
+}
+vector& vector::operator =(const vector&vec)
+{
+	if (this != &vec)
+		Size = vec.Size;
+	delete[]data;
+	data = new T[Size];
+	memcpy(data, vec.data, sizeof(T)*Size);
+	return *this;
+}
+T& vector::operator [](unsigned val)
+{
+	return data[val];
+}
+vector::~vector()
+{
+	Size = 0;
+	delete[]data;
+}
 
 int main()
 {
